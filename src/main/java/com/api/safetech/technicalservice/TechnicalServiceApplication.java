f34580a1cfc811259bd7d57e93ecc87c7b8c5be5
Debie <debiegarcia@hotmail.com>
2022-05-22 20:56:01 -0500

    Add Eureka Client

--- src/main/java/com/api/safetech/technicalservice/TechnicalServiceApplication.java
+++ src/main/java/com/api/safetech/technicalservice/TechnicalServiceApplication.java
@@ -3,11 +3,13 @@ package com.api.safetech.technicalservice;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @OpenAPIDefinition
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableEurekaClient
 public class TechnicalServiceApplication {
 
     public static void main(String[] args) {
